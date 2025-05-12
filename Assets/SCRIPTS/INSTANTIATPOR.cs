653225c89d1a50c8d05aee1e729dc24509c8bd73
Sabetay <lolasabetay@gmail.com>
2025-05-12 12:14:02 -0300

    termine

---
+++ Assets/SCRIPTS/INSTANTIATPOR.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class INSTANTIATPOR : MonoBehaviour
+{
+    public GameObject cubePreFab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CLONARCUBO();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    } 
+    public void CLONARCUBO()
+    {
+        Instantiate(cubePreFab);
+    }
+}
+
---
+++ Assets/SCRIPTS/RANDOM.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RANDOM : MonoBehaviour
+{
+    public GameObject preFab1;
+    public GameObject preFab2;
+    public GameObject preFab3;
+    public int min;
+    public int max;
+    public int randomNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ClonerRandom()
+    {
+        randomNumber = Random.Range(min,max + 1);
+        if (randomNumber == 0)
+        {
+            Instantiate(preFab1);
+        } else if (randomNumber == 1)
+        {
+            Instantiate(preFab2);
+        }else
+        {
+            Instantiate(preFab3);
+        }
+    }
+}
